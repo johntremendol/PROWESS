49741406870228a0be1668309e91a0e4a4a37ab8
John <johntremendol@gmail.com>
2026-01-03 12:39:46 +0530

    fix(ui): show names in settlement rows to resolve initials collision confusion

--- apps/splitter/components/SettlementRow.tsx
+++ apps/splitter/components/SettlementRow.tsx
@@ -24,37 +24,49 @@ const SettlementRow: React.FC<SettlementRowProps> = ({
   currency,
 }) => {
   return (
-    <div 
+    <div
       className="flex items-center py-6 px-4 border-b border-black"
       style={{ backgroundColor: '#1F1A17' }}
     >
-      {/* From Avatar */}
-      <Avatar 
-        name={from.name} 
-        size="md" 
-        variant="outline"
-      />
-      
-      {/* Left Line - Straight, no arrow */}
-      <div className="flex-1 h-px bg-[#CC342C] mx-4" />
-      
-      {/* Amount in Center */}
-      <div className="text-display text-2xl text-prowess-beige whitespace-nowrap">
-        {currency}{Math.round(amount).toLocaleString()}
+      {/* From Avatar & Name */}
+      <div className="flex flex-col items-center w-16 gap-1">
+        <Avatar
+          name={from.name}
+          size="md"
+          variant="outline"
+        />
+        <span className="text-[10px] text-prowess-grey text-center truncate w-full uppercase tracking-tighter">
+          {from.name.split(' ')[0]}
+        </span>
       </div>
-      
+
+      {/* Left Line - Straight, no arrow */}
+      <div className="flex-1 h-px bg-[#CC342C] mx-2 self-center mt-[-16px]" />
+
+      {/* Amount in Center */}
+      <div className="flex flex-col items-center self-center mt-[-16px]">
+        <div className="text-display text-2xl text-prowess-beige whitespace-nowrap">
+          {currency}{Math.round(amount).toLocaleString()}
+        </div>
+      </div>
+
       {/* Right Line - With arrow pointing to recipient */}
-      <div className="flex-1 h-px bg-[#CC342C] mx-4 relative">
+      <div className="flex-1 h-px bg-[#CC342C] mx-2 relative self-center mt-[-16px]">
         {/* Arrow Head */}
         <div className="absolute right-0 top-1/2 -translate-y-1/2 w-0 h-0 border-t-[4px] border-t-transparent border-b-[4px] border-b-transparent border-l-[6px] border-l-[#CC342C]" />
       </div>
-      
-      {/* To Avatar */}
-      <Avatar 
-        name={to.name} 
-        size="md" 
-        variant="outline"
-      />
+
+      {/* To Avatar & Name */}
+      <div className="flex flex-col items-center w-16 gap-1">
+        <Avatar
+          name={to.name}
+          size="md"
+          variant="outline"
+        />
+        <span className="text-[10px] text-prowess-grey text-center truncate w-full uppercase tracking-tighter">
+          {to.name.split(' ')[0]}
+        </span>
+      </div>
     </div>
   );
 };
